1a299ebafa5df6d77c57afbacca19fb363e35497
MSI <MSI@192.168.1.35>
2024-12-21 22:53:08 +0530

    Student.java

---
+++ src/main/java/com/example/demo/Student.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+public class Student{
+private String name;
+
+private int num;
+private String address;
+
+public String getName() {
+	return name;
+}
+
+public int getNum() {
+	return num;
+}
+
+public String getAddress() {
+	return address;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public void setNum(int num) {
+	this.num = num;
+}
+
+public void setAddress(String address) {
+	this.address = address;
+}
+
+@Override
+public String toString() {
+	return "StudentName [name=" + name + ", num=" + num + ", address=" + address + "]";
+}
+}
