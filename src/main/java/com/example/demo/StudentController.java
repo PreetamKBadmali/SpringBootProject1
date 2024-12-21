ff7c48b5b3308021d86790cd2fb7613e27a8a511
MSI <MSI@192.168.1.35>
2024-12-21 23:13:03 +0530

    commit 2

--- src/main/java/com/example/demo/StudentController.java
+++ src/main/java/com/example/demo/StudentController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class StudentController {
 @GetMapping("/getData")
 //http://localhost:8080/student/getData
+//http://localhost:8080/student/getData
 public String getUser() {
  return"student details";
 }
